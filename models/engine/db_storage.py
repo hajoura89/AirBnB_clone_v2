bdefddd0aa139748de999a791dc51ddd7f0c98d7
hajoura89 <hajarkammou@gmail.com>
2023-10-22 18:02:45 -0700

    pycode style errors

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """database storage engine"""
 
+from models.amenity import Amenity
 from models.base_model import Base
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models.amenity import Amenity
 import os
 from os import getenv
 import sqlalchemy
